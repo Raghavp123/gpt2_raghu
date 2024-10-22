80d15fde5e28c4ea3666381c0f0e8f8079b1ebee
Raghavendra <156374725+Raghavp123@users.noreply.github.com>
2024-10-22 12:29:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,21 @@ import torch
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
 # Load pre-trained GPT-2 model and tokenizer
+##https://www.it-jim.com/blog/training-and-fine-tuning-gpt-2-and-gpt-3-models-using-hugging-face-transformers-and-openai-api/
 model_gpt2 = GPT2LMHeadModel.from_pretrained("gpt2")
 tokenizer_gpt2 = GPT2Tokenizer.from_pretrained("gpt2")
 
 # Define a function for generating text
+## https://huggingface.co/docs/transformers/en/model_doc/gpt2
 def generate_text(prompt, num_tokens, temp_value):
     # Tokenize and encode the prompt
+   ## https://keras.io/api/keras_nlp/models/gpt2/gpt2_tokenizer/
     input_ids = tokenizer_gpt2.encode(prompt, return_tensors='pt')
     # Generate text with the specified temperature and length
-    generated_outputs = model_gpt2.generate(
+    ##https://huggingface.co/docs/transformers/en/model_doc/gpt2
+    ## https://github.com/huggingface/transformers/blob/main/src/transformers/models/gpt2/tokenization_gpt2.py
+    ##https://dev.to/whatminjacodes/building-a-simple-chatbot-using-gpt-model-part-2-45cn
+        generated_outputs = model_gpt2.generate(
         input_ids,
         max_length=num_tokens,
         temperature=temp_value,
@@ -23,6 +29,7 @@ def generate_text(prompt, num_tokens, temp_value):
     return tokenizer_gpt2.decode(generated_outputs[0], skip_special_tokens=True)
 
 # Streamlit App Interface
+##https://github.com/streamlit/streamlit/blob/648b30dd1dd1aab085b9fd868316d6b9d5bd9bdf/examples/interactive_widgets.py#L38
 st.title("GPT-2 Predicted and Creative texts")
 # Text input user's prompt
 user_prompt = st.text_input("Enter your prompt:", value="")
